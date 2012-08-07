547449ef2646d1e96ec79a8778ca13c2c4708ce1
vladimir <vladimir@transpacket.com>
2012-08-07 12:54:48 +0200

    Starting a simple python implementation of libnetconf

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='netconf',
+      version='0.0.1',
+      description="Python implementation of libnetconf",
+      author="Vladimir Vassilev",
+      author_email="vladimir@transpacket.com",
+      url="http://google.com",
+      packages=["netconf"],
+      license="Apache License 2.0",
+      platforms=["Posix; OS X; Windows"],
+      #classifiers=[]
+      )
