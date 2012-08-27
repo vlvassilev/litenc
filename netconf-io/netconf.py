cb0d94bddf8749b55d57dd235b0c9723e880587d
vladimir <vladimir@transpacket.com>
2012-08-27 12:52:28 +0200

    Added netconf_connect, netconf_load_config, netconf_terminate, netconf_xget_leaf_value helper functions based on the core netconf class

--- netconf-io/netconf.py
+++ netconf-io/netconf.py
@@ -9,7 +9,7 @@ class netconf:
 
     def connect(self, arg):
         #arg="server=192.168.209.31 port=830 user=root password=hadm1_123"
-        print "connecting: " + arg
+        #print "connecting: " + arg
         args = arg.split(" ");
         user="root"
         password="hadm1_123"
@@ -20,16 +20,16 @@ class netconf:
             current_pair = args[i].split("=")
             if current_pair[0] == "user":
                 user=current_pair[1]
-                print "user is " + user
+                #print "user is " + user
             if current_pair[0] == "password":
                 password=current_pair[1]
-                print "password is " + password
+                #print "password is " + password
             if current_pair[0] == "server":
                 server=current_pair[1]
-                print "server is " + server
+                #print "server is " + server
             if current_pair[0] == "port":
                 port=int(current_pair[1])
-                print "port is " + str(port)
+                #print "port is " + str(port)
 
         # now connect
         try:
@@ -55,7 +55,7 @@ class netconf:
 
         # TODO: check server's host key -- this is important.
         key = self.t.get_remote_server_key()
-        print key
+        #print key
 
         self.t.auth_password(user, password)
 
@@ -71,12 +71,12 @@ class netconf:
         return 0
 
     def rpc(self, xml):
-        print "sending: " + xml
+        #print "sending: " + xml
         try:
             data = xml + "]]>]]>"
             while data:
                 n = self.chan.send(data)
-                print "sent " + str(n)
+                #print "sent " + str(n)
                 if n <= 0:
                     return -1
                 data = data[n:]
@@ -86,12 +86,12 @@ class netconf:
             return -1
 
         #receive reply
-        print "receiving ..."
+        #print "receiving ..."
         total_data = ""
         while True:
             data = self.chan.recv(4096)
             if data:
-                print "got: " + str(data)
+                #print "got: " + str(data)
                 total_data = total_data + str(data)
             else:
                 return -1
@@ -104,8 +104,91 @@ class netconf:
         return (0,reply_xml)
 
     def terminate(self):
-        print "terminating"
+        #print "terminating"
         self.chan.close()
         self.t.close()
 
         return
+
+#helper API
+from xml.dom.minidom import parseString
+
+def netconf_connect(serverip, port, user, password):
+    my_netconf = netconf()
+    ret = my_netconf.connect('server=%(serverip)s port=%(port)d user=%(user)s password=%(password)s' % {'serverip':serverip, 'port':port, 'user':user, 'password':password})
+    if ret != 0:
+        return None
+
+    (ret, reply_xml) = my_netconf.rpc("<hello>\
+     <capabilities>\
+       <capability>urn:ietf:params:netconf:base:1.0</capability>\
+     </capabilities>\
+    </hello>")
+    if ret != 0:
+        return None
+    return my_netconf
+
+def netconf_load_config(my_netconf, config):
+    edit_config_rpc='<rpc message-id="1" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">\
+<edit-config>\
+ <target>\
+   <candidate/>\
+ </target>\
+ <default-operation>replace</default-operation>\
+%s\
+</edit-config>\
+</rpc>' % config
+
+    (ret, reply_xml) = my_netconf.rpc(edit_config_rpc)
+    if ret != 0:
+        return -1
+    reply_dom = parseString(reply_xml)
+    assert reply_dom.documentElement.tagName == "rpc-reply"
+    isok = reply_dom.getElementsByTagName("okey")
+    if isok == None:
+        return -1
+    
+    (ret, reply_xml) = my_netconf.rpc('<rpc message-id="1" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">\
+    <commit/>\
+    </rpc>')
+    if ret != 0:
+        return -1
+    reply_dom = parseString(reply_xml)
+    assert reply_dom.documentElement.tagName == "rpc-reply"
+    isok = reply_dom.getElementsByTagName("okey")
+    if isok == None:
+        return -1
+    return 0
+
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+def netconf_xget_leaf_value(my_netconf, xpath):
+    get_rpc='<rpc message-id="101"\
+  xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">\
+  <get>\
+    <filter type="xpath" select=\"%s\"/>\
+  </get>\
+ </rpc>' % xpath
+    (ret, reply_xml) = my_netconf.rpc(get_rpc)
+    if ret != 0:
+        return -1
+    reply_dom = parseString(reply_xml)
+    assert reply_dom.documentElement.tagName == "rpc-reply"
+    isdata = reply_dom.getElementsByTagName("data")
+    if isdata == None:
+        return -1
+    xpath_split = xpath.split("/")
+    value_name = xpath_split[-1]
+    value_dom = reply_dom.getElementsByTagName(value_name)[0]
+
+    return (0,getText(value_dom.childNodes))
+
+
+def netconf_terminate(my_netconf):
+    my_netconf.terminate()
+
