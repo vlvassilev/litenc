7f8d1618305761f85df29d90a05ba6c155d71e07
Vladimir Vassilev <vladimir@lightside-instruments.com>
2020-12-09 02:06:03 +0100

    Avoided some optional dependencies on counters not implemented in all model contexts

--- tntapi/example/ietf-network-interconnect-tester/test-rfc2544-throughput.py
+++ tntapi/example/ietf-network-interconnect-tester/test-rfc2544-throughput.py
@@ -34,26 +34,43 @@ def get_traffic_stats(rx_node, rx_node_port, tx_node, tx_node_port, before, afte
 	before=tntapi.strip_namespaces(before)
 	after=tntapi.strip_namespaces(after)
 
-	generated_octets=1.0*delta[tx_node][tx_node_port].out_octets
-	generated_pkts=generated_octets/(frame_size-4)
-	print total_frames
-	print generated_pkts
-	generated_pkts=total_frames
-	assert(total_frames==generated_pkts)
+	#generated_octets=1.0*delta[tx_node][tx_node_port].out_octets
+	#generated_pkts=generated_octets/(frame_size-4)
+	generated_pkts = total_frames
+	generated_octets = total_frames*frame_size
+
 	#assert(delta[tx_node][tx_node_port].out_unicast_pkts==generated_pkts)
 
-	assert(generated_octets>0)
+	#assert(generated_octets>0)
 	print("generated_octets="+str(generated_octets))
+	testframe_pkts_nodes = after.xpath("""node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/testframe-pkts"""%(rx_node, rx_node_port))
+	if(len(testframe_pkts_nodes) == 1):
+		rx_testframe_pkts=1.0*get_delta_counter(before,after,rx_node,"""/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/testframe-pkts"""%(rx_node_port))
+#		assert(rx_testframe_pkts>0)
+	else:
+		rx_testframe_pkts=None
+		assert(0)
 
-	sequence_errors=get_delta_counter(before,after,rx_node,"""/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/sequence-errors"""%(rx_node_port))
-	#sequence_errors=0
-	rx_testframe_pkts=1.0*get_delta_counter(before,after,rx_node,"""/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/testframe-pkts"""%(rx_node_port))
+	sequence_errors_nodes = after.xpath("""node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/sequence-errors"""%(rx_node, rx_node_port))
+	if(len(sequence_errors_nodes) == 1):
+		sequence_errors=get_delta_counter(before,after,rx_node,"""/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/sequence-errors"""%(rx_node_port))
+	else:
+		sequence_errors=None
+
+
 	rx_in_pkts=1.0*long(after.xpath("""node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/pkts"""%(rx_node, rx_node_port))[0].text)
 
-	latency_min = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/min"%(rx_node, rx_node_port))[0].text)
-	latency_max = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/max"%(rx_node, rx_node_port))[0].text)
-	#latency_average = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/average"%(rx_node, rx_node_port))[0].text)
-	latency_average=None
+	latency_nodes = after.xpath("""node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency"""%(rx_node, rx_node_port))
+	if(len(latency_nodes) == 1):
+		latency_min = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/min"%(rx_node, rx_node_port))[0].text)
+		latency_max = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/max"%(rx_node, rx_node_port))[0].text)
+		#latency_average = long(after.xpath("node[node-id='%s']/data/interfaces/interface[name='%s']/traffic-analyzer/state/testframe-stats/latency/average"%(rx_node, rx_node_port))[0].text)
+		latency_average=None
+	else:
+		latency_min=None
+		latency_max=None
+		latency_average=None
+
  
 	return (rx_in_pkts, rx_testframe_pkts, generated_pkts, sequence_errors, latency_min, latency_max, latency_average)
 
@@ -90,7 +107,7 @@ def trial(network, conns, yconns, test_time=60, frame_size=1500, interframe_gap=
 
 	total_frames = test_time*speed/((interframe_gap+frame_size)*8)
 
-	testframe = []
+	testframe = ""
 	if(testframe_type != []):
 		testframe = "testframe/type=%s" % testframe_type
 	print """create /interfaces/interface[name="%(name)s"]/traffic-generator -- frame-size=%(frame-size)d interframe-gap=%(interframe-gap)d total-frames=%(total-frames)s %(burst)s frame-data=%(frame-data)s %(testframe)s""" % {'name':tx_node_port,'frame-size':frame_size, 'interframe-gap':interframe_gap, 'burst':my_burst_config, 'total-frames':total_frames, 'frame-data':frame_data, 'testframe':testframe}
@@ -170,12 +187,14 @@ def main():
 	(rx_in_pkts, rx_testframe_pkts, generated_pkts, sequence_errors, latency_min, latency_max, latency_average) = trial(network, conns, yconns, test_time=int(args.test_time), frame_size=long(args.frame_size), interframe_gap=long(args.interframe_gap), tx_node=args.tx_node, tx_node_port=args.tx_node_port, rx_node=args.rx_node, rx_node_port=args.rx_node_port, src_mac_address=args.src_mac_address, dst_mac_address=args.dst_mac_address, frame_data=args.frame_data, testframe_type=args.testframe_type)
 	print("Test time:                      %8u"%(int(args.test_time)))
 	print("Generated packets:              %8u"%(generated_pkts))
+	print("Received  packets:              %8u"%(rx_testframe_pkts))
 	#print("Generated octets MB/s:          %8f"%(generated_pkts*float(args.frame_size)/(test_time*1024*1024))
 	print("Lost packets:                   %8u"%(generated_pkts-rx_testframe_pkts))
 	print("Lost packets percent:           %2.6f"%(100*float(generated_pkts-rx_testframe_pkts)/generated_pkts))
-	print("Sequence errors:                %8u"%(sequence_errors))
-	print("Sequence errors percent:        %2.6f"%(100*float(sequence_errors)/generated_pkts))
-	if(rx_testframe_pkts>0):
+	if(sequence_errors != None):
+		print("Sequence errors:                %8u"%(sequence_errors))
+		print("Sequence errors percent:        %2.6f"%(100*float(sequence_errors)/generated_pkts))
+	if(latency_min != None and rx_testframe_pkts>0):
 		print("Latency Min[nanoseconds]:       %8u"%(latency_min))
 		print("Latency Max[nanoseconds]:       %8u"%(latency_max))
 	else:
